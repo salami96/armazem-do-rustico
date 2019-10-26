31b26c79ae199c866789bbdd33f0c6530a733252
salami96 <gabriel.zanatto2@gmail.com>
2019-10-26 17:24:25 -0300

    fix .use method

--- server.ts
+++ server.ts
@@ -32,7 +32,7 @@ const DIST_FOLDER = join(process.cwd(), 'dist');
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
 const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./server/main');
 
-app.forceSSL();
+app.use(forceSSL());
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine('html', ngExpressEngine({
